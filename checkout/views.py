c6d9c476ecbede5402d74ff85d91ea2f2a069b4f
Jack Bransfield <bransfieldjack@gmail.com>
2018-12-04 02:18:14 +0000

    Updated the checkout/views.py file.

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required 
+from .forms import MakePaymentForm, OrderForm
+from django.conf import settings 
+import stripe
 
-# Create your views here.
+
+stripe_api_key = settings.STRIPE_SECRET
+
+@login_required()
+def checkout(request):
+    if request.method=='POST':
+        order_form = OrderForm(request.POST)
+        payment_form = MakePaymentForm(request.POST)
+        
+        if order_form.is_valid() and payment_form.is_valid():
+            order = order
